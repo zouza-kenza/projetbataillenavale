8c29316b53778138887cabb1cfe092def43ab607
zouza-kenza <120374566+zouza-kenza@users.noreply.github.com>
2023-12-11 17:06:19 +0100

    Update GrilleNavale.java

--- GrilleNavale.java
+++ GrilleNavale.java
@@ -1,7 +1,16 @@
 package batailleNavale;
 
 public class GrilleNavale {
-
+public GrilleNavale(int taille, int[] taillesNavires) {
+	    this(taille,taillesNavires.length);
+		placementAuto(taillesNavires);
+		
+	}
+		public GrilleNavale(int taille, int nbNavires) {
+			this.taille=taille;
+			this.nbNavires=0;
+			this.navires=new Navire[nbNavires];
+		}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
