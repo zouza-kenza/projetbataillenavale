91c111eb6e18e35f063692f05426277c49662c96
Nam DO <75475709+namdo203@users.noreply.github.com>
2023-12-11 12:28:10 +0100

    Update constructeurs

--- Navire.java
+++ Navire.java
@@ -1,6 +1,36 @@
 package batailleNavale;
 
 public class Navire {
+		private Coordonnee debut;
+	private Coordonnee fin;
+	private Coordonnee[] partiesTouchees;
+	private int nbTouchees;
+	
+	
+	//constructeur :
+	public Navire(Coordonnee debut, int longueur, boolean estVertical) {
+		this.debut = debut;
+		if (estVertical) {
+			fin = new Coordonnee(debut.getLigne() + longueur - 1, debut.getColonne());
+		}
+		else {
+			fin = new Coordonnee(debut.getLigne(), debut.getColonne() + longueur - 1);
+		}
+		partiesTouchees = new Coordonnee[longueur];
+		nbTouchees = 0;
+	}
+	
+	// méthodes :
+//	public String toString() {...}
+//	public Coordonnee getDebut() {...}
+//	public Coordonnee getFin() {...}
+//	public boolean contient(Coordonnee c) {...}
+//	public boolean touche(Navire n) {...}
+//	public boolean chevauche(Navire n) {...}
+//	public boolean recoitTir(Coordonnee c) {...}
+//	public boolean estTouche(Coordonnee c) {...}
+//	public boolean estTouche() {...}
+//	public boolean estCoule() {...}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
